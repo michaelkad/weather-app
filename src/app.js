fdf484ca9fdbd2a269d6da17e19090a3d32d80aa
mkadiayi <michaelkadiayi@gmail.com>
2020-07-02 15:13:02 -0500

    modified package.json

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const weather = require('./weather')
 app.use(express.static(path.join(__dirname, '../public')))
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
-
+const port = process.env.PORT || 3000
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
@@ -58,6 +58,6 @@ app.get('/weather', (req, res) =>{
 app.get('*', (req, res) => {
     res.send('404 page')
 })
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on port ' + port)
 })
